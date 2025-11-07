597f87cc48951f38636e4b740608f6711ba406e5
vivid-dev-28 <vivid.dhananjaykhodaskar@gmail.com>
2025-11-07 14:48:09 +0530

    test commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,7 +4,7 @@ import './styles.css'
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
-}
+} 
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
